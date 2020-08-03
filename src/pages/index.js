ae4602d17d62ff9484c4c660433ede38869996c7
Akhil Waghmare <akhilwaghmare@gmail.com>
2020-08-03 16:13:02 -0400

    Completed desktop index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
 import React from "react"
 import { Link } from "gatsby"
 
@@ -5,17 +8,91 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import DesignImg from "../images/design-img.png"
+import ArtImg from "../images/art-img.png"
+
+const indexPageStyle = css`
+  .text {
+    margin-top: 64px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .display {
+      margin-bottom: 36px;
+    }
+  }
+
+  .links {
+    margin-top: 48px;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 96px;
+
+    .link-card {
+      position: relative;
+      border-radius: 24px;
+      overflow: hidden;
+
+      .image {
+        width: 100%;
+        padding-top: 66%;
+        background-size: cover;
+        background-position: center;
+        background-color: rgba(0,0,0,0.7);
+
+        filter: blur(4px);
+        -webkit-filter: blur(4px);
+
+        transition: all 0.3s ease-out;
+
+        &:hover {
+          background-color: transparent;
+          filter: blur(0px);
+          -webkit-filter: blur(0px);
+        }
+
+        &#design {
+          background-image: url(${DesignImg});
+        }
+
+        &#art {
+          background-image: url(${ArtImg});
+        }
+      }
+
+      h1 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    }
+  }
+`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="content" css={indexPageStyle}>
+      <div className="text">
+        <span className="display">Calvin Stewart</span>
+        <h3>Product Designer. UX/UI Designer. Artist.</h3>
+      </div>
+      <div className="links">
+        <div className="link-card">
+          <div className="image" id="design"></div>
+          <h1>Design</h1>
+        </div>
+        <div className="link-card">
+          <div className="image" id="art"></div>
+          <h1>Art</h1>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
   </Layout>
 )
 
