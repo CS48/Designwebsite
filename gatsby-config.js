606542f8f71f16124480e4b8a52505002051b04d
Akhil Waghmare <akhilwaghmare@gmail.com>
2020-08-03 13:14:22 -0400

    Started updating header component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Calvin Stewart`,
+    description: `Official portfolio for Calvin Stewart's design and art work.`,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
